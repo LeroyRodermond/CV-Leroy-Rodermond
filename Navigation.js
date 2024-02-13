3248881aeb9630891d0bff64c939b1320a2b7a64
Leroy <116569478+LeroyRodermond@users.noreply.github.com>
2024-02-13 12:22:29 +0100

    Done

---
+++ Navigation.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("cvButton").addEventListener("click", function() {
+        window.location.href = "cv.html";
+    });
+
+    document.getElementById("contactButton").addEventListener("click", function() {
+        window.location.href = "contactpagina/contact.html";
+    });
+
+    document.getElementById("liveChatButton").addEventListener("click", function() {
+        window.location.href = "livechat.html/chat";
+    });
+});
