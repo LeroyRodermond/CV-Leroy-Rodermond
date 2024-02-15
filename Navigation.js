947bb63e6a9386813814323fb5002364de647904
Leroy <116569478+LeroyRodermond@users.noreply.github.com>
2024-02-15 15:24:12 +0100

    Head Page

--- Navigation.js
+++ Navigation.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("cvButton").addEventListener("click", function() {
-        window.location.href = "index.html";
+        window.location.href = "../curriculumvitae/cv.html";
     });
 
     document.getElementById("contactButton").addEventListener("click", function() {
-        window.location.href = "contactpagina/contact.html";
+        window.location.href = "../contactpagina/contact.html";
     });
 
     document.getElementById("liveChatButton").addEventListener("click", function() {
-        window.location.href = "livechat/chat";
+        window.location.href = "../livechat/chat.html";
     });
 });
