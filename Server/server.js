5674dffcd44e766ff6d34aa098ee48f37508f28c
Leroy <116569478+LeroyRodermond@users.noreply.github.com>
2024-02-26 16:19:12 +0100

    Packages

--- Server/server.js
+++ Server/server.js
@@ -1,32 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+require("dotenv").config();
+
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 // Middleware om JSON en URL-encoded body's te verwerken
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// Stel e-mailconfiguratie in
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'jouw_email@gmail.com', // Voeg hier je e-mailadres in
-        pass: 'jouw_wachtwoord' // Voeg hier je wachtwoord in
-    }
-});
+
+
+
 
 // Route voor het verwerken van het contactformulier
-app.post('/contact', (req, res) => {
+app.post('/contact', async (req, res) => {
     const { firstname, lastname, email, telefoonnummer, onderwerp, bericht } = req.body;
 
+
+    const transporter = nodemailer.createTransport({
+        // Stel e-mailconfiguratie in
+          service: 'gmail',
+          auth: {
+              user: process.env.EMAIL_USER,
+              pass: process.env.EMAIL_PASS
+          }
+      });
+
+      
     // Bouw het e-mailbericht
     const mailOptions = {
-        from: 'jouw_email@gmail.com',
+        from: `"${firstname}, ${lastname}"`,
         to: 'l.g.h.rodermond@gmail.com',
-        subject: 'Nieuw bericht van contactformulier',
+        subject: `${onderwerp}`,
         text: `
             Naam: ${firstname} ${lastname}
             E-mail: ${email}
@@ -38,6 +48,15 @@ app.post('/contact', (req, res) => {
         `
     };
 
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log("Email succes");
+        res.send("succsess")
+    } catch(error) {
+        console.error(error);
+    }
+
+/*
     // Verstuur de e-mail
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
@@ -48,6 +67,7 @@ app.post('/contact', (req, res) => {
             res.send('Bericht is verzonden!');
         }
     });
+    */
 });
 
 // Start de server
